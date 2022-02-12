82ed911b8e2169360f35ffd7a59cc859d1411de8
Francesco Maestrelli <framae26@gmail.com>
2022-02-12 11:42:44 +0100

    todo in todos

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*Descrizione: Rifare l'esercizio della to do list.
 Questa volta però ogni todo sarà un oggetto, formato da due proprietà:
 - text, una stringa che indica il testo del todo
- - done, un booleano (true/false) che indica se il todo è stato fatto oppure no 
+- done, un booleano (true/false) che indica se il todo è stato fatto oppure no 
 MILESTONE 1 
 Stampare all'interno di una lista, un item per ogni todo. Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 MILESTONE 2 
@@ -19,7 +19,23 @@ const app = new Vue (
     {
         el: '#root',
         data: {
+            newToDo: '',
+            toDos: [
+                {
+                    text: 'eseguire la consegna',   
+                    done: true,
+                },
 
+                {
+                    text: 'fare la spesa',   
+                    done: true,
+                },
+
+                {
+                    text: 'vedere serie Netflix',   
+                    done: true,
+                }
+            ]
         }
     }
 )
